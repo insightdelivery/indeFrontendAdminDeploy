49bc7063b26ea3d08c7bb270445f82f80538bce5
Hyoung Jin Nam <pronhj@gmail.com>
2026-02-27 14:38:13 +0900

    게시판 작업

--- _next/static/zJgOKmttscahxGwKfH2JQ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Farticles\u002F[id]","\u002Fadmin\u002Farticles\u002F[id]\u002Fedit","\u002Farticles\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
